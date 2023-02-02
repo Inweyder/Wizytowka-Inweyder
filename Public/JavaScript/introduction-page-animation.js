9e71d23cc25d3007f91fe6cb890e09c05adeb0ff
IgorPatrykPraktyki <patrykkepka5@gmail.com>
2023-02-02 13:01:37 +0100

    about-me

---
+++ Public/JavaScript/introduction-page-animation.js
@@ -0,0 +1,19 @@
+// Introduction Page Animation
+
+let box = document.querySelector("#introduction-page");
+
+box.firstElementChild.classList.remove("introduction-page__box");
+box.firstElementChild.classList.add("introduction-page__box--off");
+
+let interval = setInterval(function() { box.getBoundingClientRect().top <=100 ? changeClass() : null}, 1000/60);
+
+function changeClass() {
+    box.firstElementChild.classList.remove("introduction-page__box--off");
+    box.firstElementChild.classList.add("introduction-page__box");
+    clearInterval(interval);
+
+}
+
+
+
+
--- Public/JavaScript/menu.js
+++ Public/JavaScript/menu.js
@@ -1,15 +1,15 @@
 let body = document.querySelector(".title-screen__title");
-let box = document.querySelector(".header");
+let head = document.querySelector(".header");
 
 let intervalbox = setInterval(function() { body.getBoundingClientRect().bottom <=300 ? boxOn() : null}, 1000/60);
 
 function boxOn() {
-    box.setAttribute("style", "background-color: #08091c;");
+    head.setAttribute("style", "background-color: #08091c;");
     console.log("1");
 }
 
 let intervalboxoff = setInterval(function() { body.getBoundingClientRect().bottom >=300 ? boxOff() : null}, 1000/60);
 function boxOff(){
-    box.setAttribute("style", "background-color: none;");
+    head.setAttribute("style", "background-color: none;");
     console.log("2");
 }

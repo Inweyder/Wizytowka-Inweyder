3cec9ff3c9eeef09a5b11f953350076f60d285d1
Inweyder <stas1103s@gmail.com>
2023-02-03 11:27:42 +0100

    card-flip-animation

---
+++ Public/JavaScript/cards-animation.js
@@ -0,0 +1,17 @@
+const card1 = document.querySelector('.programming-languages__cards-box-1');
+
+card1.addEventListener('click', function(){
+    card1.classList.toggle('is-flipped');
+})
+
+const card2 = document.querySelector('.programming-languages__cards-box-2');
+
+card2.addEventListener('click', function(){
+    card2.classList.toggle('is-flipped');
+})
+
+const card3 = document.querySelector('.programming-languages__cards-box-3');
+
+card3.addEventListener('click', function(){
+    card3.classList.toggle('is-flipped');
+})
